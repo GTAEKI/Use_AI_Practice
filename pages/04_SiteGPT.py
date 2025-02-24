f2f5bccddc7f36350f17cfc657e68648fd44a6a6
BaeKyungTaek <kyungtaek0204@gmail.com>
2025-02-24 11:22:53 +0900

    [Add] AsyncChromiumLoader

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,3 +1,18 @@
 import streamlit as st
+from langchain.document_loaders import AsyncChromiumLoader
+from langchain.document_transformers import Html2TextTransformer
 
 st.title("Site GPT")
+
+html2text_transformer = Html2TextTransformer()
+
+with st.sidebar:
+    url = st.text_input("Write down a URL", placeholder="https://example.com")
+
+if url:
+    #async chromium loader
+    loader = AsyncChromiumLoader([url])
+    docs = loader.load()
+    transformed = html2text_transformer.transform_documents(docs)
+    st.write(transformed)
+
